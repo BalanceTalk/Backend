d34d5ffa3f6c4d75919e27927d8b857b6b77fbb0
Hanjaemo <hjm2530@gmail.com>
2023-10-31 01:12:23 +0900

    feat: 게임 좋아요 기능 구현

---
+++ src/main/java/com/cnusw/balancetalk/domain/game/entity/GameLikes.java
@@ -0,0 +1,38 @@
+package com.cnusw.balancetalk.domain.game.entity;
+
+import com.cnusw.balancetalk.domain.comment.entity.Comment;
+import com.cnusw.balancetalk.domain.member.entity.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class GameLikes {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "game_likes_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
