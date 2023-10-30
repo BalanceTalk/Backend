576925ae2893cc2d555174672632a45d4d3c076a
Hanjaemo <hjm2530@gmail.com>
2023-10-30 16:57:58 +0900

    fix(GameService, SecutiryConfig): 카테고리별 게임 조회 버그 수정 및 경로별 접근 권한 수정

--- src/main/java/com/cnusw/balancetalk/global/config/SecurityConfig.java
+++ src/main/java/com/cnusw/balancetalk/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // token을 사용하는 방식이기 때문에 csrf를 disable로 설정한다.
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
 
                 // 응답코드 변환 403 -> 401
                 .exceptionHandling(exceptionHandling -> exceptionHandling
@@ -40,24 +41,19 @@ public class SecurityConfig {
                 // HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
                 .authorizeHttpRequests(authorizeHttpRequests ->
                                 authorizeHttpRequests
-                                        // 지정한 경로에 대한 요청은 인증없이 접근을 허용
-//                                .requestMatchers("/members/login", "/members/join", "/members/info").permitAll()
-                                        .requestMatchers(
-                                                new AntPathRequestMatcher("/v3/api-docs/**"),
-                                                new AntPathRequestMatcher("/swagger-ui/**"),
-                                                new AntPathRequestMatcher("/swagger-resources/**"),
-                                                new AntPathRequestMatcher("/members/login"),
-                                                new AntPathRequestMatcher("/members/join"),
-                                                new AntPathRequestMatcher("/members/info"),
-                                                new AntPathRequestMatcher("/h2-console/**"),
-                                                new AntPathRequestMatcher("/games/**"),
-                                                new AntPathRequestMatcher("/comment/**")
-                                        ).permitAll()
-//                                .requestMatchers("/members/info").authenticated() // 인증된 사용자에게만 접근 허용
-                                        .requestMatchers(new AntPathRequestMatcher("/members/info")).authenticated()
-                                        // 나머지 요청은 모두 인증 필요
-                                        .anyRequest().authenticated()
 
+                                        // 인증된 사용자에게만 접근 허용
+                                        .requestMatchers(
+                                                new AntPathRequestMatcher("/members/info"),
+                                                new AntPathRequestMatcher("/games/create"),
+                                                new AntPathRequestMatcher("/games/{id}/report"),
+                                                new AntPathRequestMatcher("/games/{id}/comment"),
+                                                new AntPathRequestMatcher("/comment/**"),
+                                                new AntPathRequestMatcher("/games/{gameId}/vote")
+                                        ).authenticated()
+
+                                        // 나머지 요청은 인증없이 접근을 허용
+                                        .anyRequest().permitAll()
                 )
 
                 // 세션을 사용하지 않기 때문에 STATELESS로 설정
